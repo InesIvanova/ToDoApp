f8315c1a87f02d8104e25e6c09205e1f6ed34f96
Ines Ivanova <ines.iv.ivanova@gmail.com>
2022-10-23 17:27:50 +0300

    Add code for editing the task

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ast
 import json
 from tkinter import (
     Tk,
@@ -8,7 +9,7 @@ from tkinter import (
     IntVar,
     BooleanVar,
     Checkbutton,
-    END,
+    END, INSERT,
 )
 from tkinter.scrolledtext import ScrolledText
 from tkinter.ttk import Combobox
@@ -16,13 +17,49 @@ from tkinter.ttk import Combobox
 from tkcalendar import DateEntry
 
 
+mapper = {1: "Minor", 2: "Medium", 3: "High"}
+
+
 def clear_view(tk):
     for el in tk.grid_slaves():
         el.destroy()
 
 
-def edit_task(task):
-    pass
+def edit_task(task, tk):
+    task = ast.literal_eval(task)
+    clear_view(tk)
+    Label(tk, text="Enter your task name: ").grid(row=0, column=0, padx=20, pady=20)
+    name = Entry(tk)
+    name.delete(0, END)
+    name.insert(0, task['title'])
+    name.grid(row=0, column=1, padx=20, pady=20)
+    Label(tk, text="Due date: ").grid(row=1, column=0, padx=20, pady=20)
+    date = DateEntry(tk)
+    date.delete(0, END)
+    date.insert(0, task['due_date'])
+    date.grid(row=1, column=1, padx=20, pady=20)
+    Label(tk, text="Description: ").grid(row=2, column=0, padx=20, pady=20)
+    description = ScrolledText(tk, width=20, height=10)
+    description.insert(INSERT, task['description'])
+    description.insert(END, "")
+    description.grid(row=2, column=1, padx=20, pady=20)
+    Label(tk, text=f"Select priority, current is {mapper[task['priority']] if task['priority'] else None}").grid(row=3, column=0, padx=20, pady=20)
+    s = IntVar()
+    rad1 = Radiobutton(tk, text='Low', value=1, variable=s)
+    rad2 = Radiobutton(tk, text='Medium', value=2, variable=s)
+    rad3 = Radiobutton(tk, text='High', value=3, variable=s)
+
+    rad1.grid(column=1, row=3)
+    rad2.grid(column=2, row=3)
+    rad3.grid(column=3, row=3)
+    Label(tk, text="Check if completed: ").grid(row=4, column=0, padx=20, pady=20)
+    Label(tk, text="Check if completed: ").grid(row=4, column=0, padx=20, pady=20)
+    chk_state = BooleanVar()
+    chk_state.set(task["is_completed"])  # set check state
+    chk = Checkbutton(tk, text='Choose', variable=chk_state, onvalue=True, offvalue=False)
+    chk.grid(column=1, row=4)
+
+    Button(tk, text="Cancel", bg="black", fg="white", command=lambda: main_view(tk)).grid(row=5, column=1, padx=100, pady=100)
 
 
 def delete_task(task):
@@ -45,7 +82,7 @@ def list_tasks_view(tk):
         text="Edit task",
         bg="yellow",
         fg="black",
-        command=lambda: edit_task(dd.get()),
+        command=lambda: edit_task(dd.get(), tk),
     ).grid(row=2, column=0)
     Button(
         tk,
